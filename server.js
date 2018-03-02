accc37c051627565ff7f0a21d3011f6b7d16e54e
GreenCoat <nickscunningham@gmail.com>
2018-03-01 18:26:52 -0600

    Finished stitching things together

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var PORT = process.env.PORT || 8000;
 var db = require("./models");
 
 app.use(bodyParser.json());
-app.use(bodyPaser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 require("./routes/api-routes.js")(app);
 
