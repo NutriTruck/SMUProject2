03a222dfc2150a12a0958e0a521f291065d1c4eb
Nedyk <nnyatome@gmail.com>
2018-03-03 12:21:22 -0600

    changes to login.js, register.js, and html pages

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -57,3 +57,85 @@
 // 		init();
 // 	});
 // });
+
+$(document).ready(function(){
+	var email =""; 
+	var password = "";
+
+	$("#failed").hide();
+	$("#signIn").on("click", function(event){
+		event.preventDefault(); 
+		if(".form-control" !== ""){
+			var email = $("#password").val().trim();
+			var password = $("#email").val().trim();
+			console.log( email + password)
+			if(email !== "" && password !== ""){
+			window.location.href = "profile.html";
+			}
+			 
+		}else{
+
+			$("#failed").show();
+		}
+
+		$("#password").val(""); 
+		$("#email").val("")
+
+	});
+
+
+function encrypt()
+{
+var password = $("#email").val().trim();
+var hide=$('#hide').val();
+if(password=="")
+{
+document.getElementById('err').innerHTML='Error:Password is missing';
+return false;
+}
+else
+{
+document.getElementById("hide").value = document.getElementById("password").value;
+var hash = CryptoJS.MD5(password);
+document.getElementById('password').value=hash;
+return true;
+}
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public/assets/javascript/register.js
+++ public/assets/javascript/register.js
@@ -1,24 +1,31 @@
-
+$(document).ready(function(){
 $("#required").hide()
 $("#register").on("click", function(event){
-	if(".form-group" !== ""){
+	event.preventDefault();
+	if("form-control" !== ""){
+		console.log("test");
 		//post to the db
-		event.preventDefault();
-		var name = $("#firstname").val().trim();
-		 $("#lastname").val().trim();
-		$("#email").val().trim();
-		$("#password").val().trim();
+		
+		var firstName = $("#firstname").val().trim();
+		var lastName = $("#lastname").val().trim();
+		var email = $("#email").val().trim();
+		var password = $("#password").val().trim();
 		console.log(
 			
 		name
 			);
+		if(firstname !== "" && lastName !== "" && email !== "" && password !== ""){
+			window.location.href = "add.html";
+		}
+	
+	}else{
+		$("#required").show()
+
 		$("#firstname").val("")
 		$("#lastname").val("")
 		$("#email").val("")
-		$("#fz").val("")
-		window.location.href = "login.html";
-		
-	}else{
-		$("#required").show()
+		$("#password").val("")
 };
+})
+
 })
