dc94ad8db3fa85b212ff4d38d1054da3898a2573
GreenCoat <nickscunningham@gmail.com>
2018-03-06 18:48:28 -0600

    Added start of a seed file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO gifts (gift, gender, age, hobbies, likes) VALUES
+('Puppy', 'Male', 20, 'walking;running', 'puppies;outdoors'),
+('Roses', 'Female', 18, 'reading;sewing;gardening', 'roses;red')
