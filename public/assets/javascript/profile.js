6865ee85dd9910c7f8a55c97150b1fb3e3b75978
GreenCoat <nickscunningham@gmail.com>
2018-03-08 15:38:42 -0600

    Cleaned up some formatting on profile page

--- public/assets/javascript/profile.js
+++ public/assets/javascript/profile.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 	$.get("/api/user_data").then(function(data){
-		$("#username").html("Hello "+data.firstname+" "+data.lastname);
+		$("#username").html("Welcome, "+data.firstname+" "+data.lastname);
 
 		$.get("/api/giftee/"+data.id, function(res){
 			if(res.length == 0){
-
+				$("#gifteesTable").html("<p>It looks like you haven't saved any requests yet!</p>");
 			}else{
 				for(var i = 0; i < res.length; i++){
-					$("#gifteesTable").append("<tr>"+
-	    									  "<td>"+res[i].name+"</td>"+ 
-	    									  "<td><button class='request btn btn-primary' data-id='"+res[i].id+"'>Request a gift</button></td>"+
-	    									  "<td><button class='delete btn btn-danger' data-id='"+res[i].id+"'>Delete</button></td>"+
-	  										  "</tr>");
+					var date = new Date(res[i].createdAt);
+					console.log(date);
+					var dateStr = date.getMonth()+1 + "/" + date.getDate();
+
+					$("#gifteesTable").append("<div style='padding: 10px'>"+
+	    									  "<div style='display: inline-block; margin-right: 10px'>"+res[i].name+" on "+dateStr+"</div>"+ 
+	    									  "<button style='margin: 10px' class='request btn btn-primary' data-id='"+res[i].id+"'>Request a gift</button>"+
+	    									  "<button style='margin: 10px' class='delete btn btn-danger' data-id='"+res[i].id+"'>Delete</button>"+
+	  										  "</div>");
 				}
 			}
 		});
@@ -23,6 +27,10 @@ $(document).ready(function(){
 			{type: 'DELETE'}).then(function(){
 				location.reload();
 		});
+	});
+
+	$(document).on("click", ".request", function(evt){
+		window.location.href = "request";
 	});
 
 });
